1cf5be15cf53d96c1d28faf2fe2359ffb00646ec
python240419 <51232248+python240419@users.noreply.github.com>
2019-08-09 10:51:17 +0300

    Update getter_setter.py

--- getter_setter.py
+++ getter_setter.py
@@ -1,7 +1,9 @@
 class Animal:
     def __init__(self, name, age):
+        self.__name = None # to avoid crash
+        self.__age = None # to avoid crash
         self.name = name
-        self.__age = age
+        self.age = age
     def __str__(self):
         return f'Animal {self.name} {self.__age}'
     '''
@@ -14,10 +16,20 @@ class Animal:
     @property
     def age(self):  # getter
         return self.__age
+
     @age.setter
     def age(self, value):
         if value >= 0:  # setter
             self.__age = value
-dolphine = Animal('Lucky', 1.5)
+    @property
+    def name(self):  # getter
+        return self.__name
+
+    @age.setter
+    def name(self, value):
+        if len(value) >= 2:  # setter
+            self.__name = value
+
+dolphine = Animal('Lucky', 200)
 dolphine.age = -200
-print(dolphine)
+print(dolphine.name)
