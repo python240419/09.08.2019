75068c6a8bc9e8cb69661f2477536084f1fcb937
python240419 <51232248+python240419@users.noreply.github.com>
2019-08-09 12:01:13 +0300

    Create super.py

---
+++ super.py
@@ -0,0 +1,35 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return f'name : {self.name}'
+
+class Dog(Animal):
+    def __init__(self, name, color):
+        # 1. code duplication
+        # self.name = name
+
+        # 2. super() -- best!
+        super().__init__(name)
+
+        # 3. parent class Name
+        #Animal.__init__(self, name)
+
+        self.color = color
+
+    def __str__(self):
+        return super().__str__() + f' Dog {self.color} '
+
+rexy = Dog('rexy', 'black')
+print(rexy)
+
+# exercise:
+# create Employee class
+# __init__ (self, name, address, salary)
+# create Manager class
+# create salary getter/setter
+# to avoid negative salary
+# implement __str__
+# __init__ (self, name, address, salary, numberOfEmployeesBenieth)
+#   call super() ...
+#   implement __str__  and call super()__str__
